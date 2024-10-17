850c51c4e71041f92aa395fdf10f27bea5e7bb27
joanne <joannembula33@gmail.com>
2024-10-17 16:02:14 +0300

    done

---
+++ task-master-frontend/src/pages/ProjectPage.js
@@ -0,0 +1,14 @@
+// src/components/ProjectPage.js
+import React from 'react';
+import ProjectList from '../components/ProjectList';
+
+const ProjectPage = () => {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <ProjectList />
+    </div>
+  );
+};
+
+export default ProjectPage;
---
+++ task-master-frontend/src/pages/TaskPage.js
@@ -0,0 +1,14 @@
+// src/components/TaskPage.js
+import React from 'react';
+import TaskList from './TaskList';
+
+const TaskPage = () => {
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <TaskList />
+    </div>
+  );
+};
+
+export default TaskPage;
