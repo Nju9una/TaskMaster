01b1f1750db7692b9c6d8ee263f02513625c70c5
bridget <bridget.mburu.w@gmail.com>
2024-10-18 07:48:19 +0300

    JWT

--- server/app.py
+++ server/app.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask
 from flask_migrate import Migrate 
 from flask_restful import Resource, Api
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
+from dotenv import load_dotenv
+from flask_jwt_extended import JWTManager
 
 
 from models import db
@@ -15,15 +18,21 @@ from resources.user import  UserResource, LoginResource
 # Flask instance 
 app = Flask(__name__)
 
+load_dotenv()
+
 # Configure the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///taskmanagement.db'
 app.config['SQLALCHEMY_ECHO'] = True
+
+app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET_KEY')
 
 # Create the database and migrate objects
 migrate = Migrate(app, db)
 db.init_app(app)
 
 CORS(app)
+
+jwt = JWTManager(app)
 
 bcrypt = Bcrypt(app)
 
